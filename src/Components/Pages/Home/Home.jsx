8db729263a3ebc119d2e30c48cd46684c00b32a1
asif ramzan <asiframzan2021@gmail.com>
2024-02-20 10:15:35 +0500

    price lowered, best price and top search tag added to carousel

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "antd/es/typography/Typography";
 import AdvanceFilter from "./AdvanceFilter";
 import ContactUs from "./ContactUs";
@@ -6,8 +6,20 @@ import MostWantedCat from "./MostWantedCat";
 import AppButton from "../../Reuseable/Button/Button";
 import CarouselCard from "../../Reuseable/CarouselCard";
 import cardata from "../../../../cardata.json"
+import { Button, Divider } from "antd";
+import Link from "antd/es/typography/Link";
 
 const Home = () => {
+  const [showData, setShowData] = useState(4);
+  const [showData2, setShowData2] = useState(4);
+
+  const handleShowMore = () => {
+    setShowData(showData + 4);
+  }
+  const handleShowMore2 = () => {
+    setShowData2(showData2 + 4);
+  }
+  
   return (
     <div className="bg-white pb-4">
       <Typography className="text-3xl font-bold py-3">
@@ -19,16 +31,36 @@ const Home = () => {
         <ContactUs />
         <MostWantedCat />
       </div>
+      <div className="flex justify-between">
       <Typography className='text-black text-xl font-semibold'>Newsest Cars</Typography>
+      <Link className="showAllLink flex items-center gap-2 font-semibold text-lg">Show all <img src="./icons/arrow-right.svg" alt="right" /></Link>
+      </div>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 mt-3">
-        {cardata.map((data)=>{
-  const {id, title, varient, year, type, milage, priceLowered, price, lowprice, imageSource } = data
+        {cardata.slice(0,showData).map((data)=>{
+          const {id, title, varient, year, type, milage, priceLowered, price, lowprice, bestPrice, topsearch, imageSource } = data
+
+          return <div key={id}>
+            <CarouselCard cardata={data} id={id} title={title} varient={varient} year={year} type={type} milage={milage} priceLowered={priceLowered} price={price} lowprice={lowprice} bestPrice={bestPrice} topsearch={topsearch} imageSource={imageSource} />
+          </div>
+        })}
+         </div>
+         <Button className="flex justify-center items-center font-bold border-none shadow-none overflow-hidden w-full text-lg my-5" onClick={handleShowMore}>Show more {<img src='./icons/expand.png' alt="showmore" className="w-5 mt-1 ml-2" />}</Button>
+         <Divider type="horizontal" />
+         <div className="flex justify-between">
+      <Typography className='text-black text-xl font-semibold'>Most wanted</Typography>
+      <Link className="showAllLink flex items-center gap-2 font-semibold text-lg">Show all <img src="./icons/arrow-right.svg" alt="right" /></Link>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 mt-3">
+        {cardata.slice(0,showData2).map((data)=>{
+          const {id, title, varient, year, type, milage, priceLowered, price, lowprice, imageSource } = data
 
           return <div key={id}>
             <CarouselCard cardata={data} id={id} title={title} varient={varient} year={year} type={type} milage={milage} priceLowered={priceLowered} price={price} lowprice={lowprice} imageSource={imageSource} />
           </div>
         })}
          </div>
+         <Button className="flex justify-center items-center font-bold border-none shadow-none overflow-hidden w-full text-lg my-5" onClick={handleShowMore2}>Show more {<img src='./icons/expand.png' alt="showmore" className="w-5 mt-1 ml-2" />}</Button>
+      
     </div>
   );
 };
--- src/Components/Reuseable/CarouselCard.jsx
+++ src/Components/Reuseable/CarouselCard.jsx
@@ -5,7 +5,7 @@ import AppButton from "./Button/Button";
 
 const CarouselCard = (data) => {
   const [heartFilled, setHeartFilled] = useState(false);
-  const {id, title, varient, year, type, milage, priceLowered, price, lowprice, imageSource } = data
+  const {id, title, varient, year, type, milage, priceLowered, price, lowprice, bestPrice, topsearch, imageSource } = data
 
   const onChange = (currentSlide) => {
     console.log(currentSlide);
@@ -24,18 +24,18 @@ const CarouselCard = (data) => {
     
           
         </Carousel>
-        <div className="flex flex-col absolute right-1 top-1 gap-1">
-          {heartFilled ? (<span className="bg-slate-700 bg-opacity-65 w-8 rounded-full h-8  flex justify-center items-center">
-            <img src="./icons/heartfilled.svg" alt="heart" onClick={() => setHeartFilled(!heartFilled)}/></span>) :
-            (<span className="bg-slate-700 bg-opacity-65 h-8 w-8 rounded-full flex justify-center items-center"> <img src="./icons/heart.svg"
-                alt="heart"
+        <div className="flex flex-col absolute right-1 top-1 gap-2">
+          {heartFilled ? (<span className="bg-slate-700 bg-opacity-65 w-6 md:w-8 rounded-full h-6 md:h-8  flex justify-center items-center">
+            <img src="./icons/heartfilled.svg" alt="heart" className="w-5 md:w-6" onClick={() => setHeartFilled(!heartFilled)}/></span>) :
+            (<span className="bg-slate-700 bg-opacity-65 h-6 md:h-8 w-6 md:w-8 rounded-full flex justify-center items-center"> <img src="./icons/heart.svg"
+                alt="heart" className="w-5 md:w-6"
                 onClick={() => setHeartFilled(!heartFilled)}
               />
             </span>
           )}
 
-          <span className="bg-slate-700 bg-opacity-65 rounded-full h-8 w-8 flex justify-center items-center">
-            <img src="./icons/share-2.svg" alt="share" />
+          <span className="bg-slate-700 bg-opacity-65 rounded-full h-6 md:h-8 w-6 md:w-8 flex justify-center items-center">
+            <img src="./icons/share-2.svg" className="w-5 md:w-6" alt="share" />
           </span>
         </div>
         
@@ -58,10 +58,16 @@ const CarouselCard = (data) => {
           <Divider type="vertical" />
           <span className="inline-flex">{milage}</span>
         </div>
-        {priceLowered ? <Tag className="mx-2 bg-[#F2F8F9] rounded-lg mb-2 text-[#61B4E7] border-none font-bold">
+        {(priceLowered === true) ? <Tag className="mx-2 bg-[#F2F8F9] rounded-lg mb-2 text-[#61B4E7] border-none font-bold">
         price lowered
-        </Tag> : ""  }
-        
+        </Tag> : (bestPrice === true) ? <Tag className="mx-2 bg-[#f1c5c5] rounded-lg mb-2 text-red-700 border-none font-bold">
+        Best Price
+        </Tag> : (topsearch === true) ? <Tag className="mx-2 bg-[#F2F8F9] rounded-lg mb-2 text-[#4eaeea] border-none font-bold">
+        Top search
+        </Tag>: <Tag className="mx-2 bg-[#F2F8F9] rounded-lg mb-2 text-[#61B4E7] invisible border-none font-bold">
+        price lowered
+        </Tag>  }
+      
         <div className="flex flex-row justify-between items-center px-2">
           <span className="flex justify-start items-center">
             <Typography className="font-bold text-xs md:text-lg"> ${price}</Typography>
